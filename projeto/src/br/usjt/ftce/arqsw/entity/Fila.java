c0ba0d5ac2518e6b7b876d2ad01cbda4e3dd755f
816118950 <816118950@MOCLI10213.animaedu.intranet>
2018-02-21 22:21:31 -0300

    projeto

---
+++ projeto/src/br/usjt/ftce/arqsw/entity/Fila.java
@@ -0,0 +1,22 @@
+package br.usjt.ftce.arqsw.entity;
+
+/**
+ * @author 816118950
+ *
+ */
+public class Fila {
+	private int id;
+	private string nome;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public string getNome() {
+		return nome;
+	}
+	public void setNome(string nome) {
+		this.nome = nome;
+	}
+}
